7c915520a9b0d8a37ee9c181463916b7e38d3585
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-08-31 15:32:36 +0200

    added post route

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,24 @@
+const axios = require('axios');
+require('dotenv').config({ path: '.env' });
+const api_key = process.env.API_KEY;
+
 exports.renderHomePage = (req, res) => {
   res.render('index');
+};
+
+exports.getWeather = (req, res) => {
+  const city = req.body.city;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render('index', {
+        weather: `It is currently ${response.data.main.temp} in ${city}`,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 exports.renderAboutPage = (req, res) => {
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('./controllers/controller');
 
 router.get('/', controller.renderHomePage);
-
+router.post('/', controller.getWeather);
 router.get('/about', controller.renderAboutPage);
 
 module.exports = router;
