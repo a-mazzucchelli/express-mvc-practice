874a066a41406a20e31dcf3d11cf39c4a76260db
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-08-31 15:56:24 +0200

    added Weather module

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 require('dotenv').config({ path: '.env' });
 const api_key = process.env.API_KEY;
 
+const Weather = require('../module/Weather');
+
 exports.renderHomePage = (req, res) => {
   res.render('index');
 };
@@ -9,16 +11,28 @@ exports.renderHomePage = (req, res) => {
 exports.getWeather = (req, res) => {
   const city = req.body.city;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
-  axios
-    .get(url)
-    .then((response) => {
-      res.render('index', {
-        weather: `It is currently ${response.data.main.temp} in ${city}`,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+
+  const weather = new Weather(req.body.city);
+  weather.validateUserInput();
+
+  if (weather.errors.length) {
+    res.render('index', {
+      error: weather.errors.toString(),
     });
+  } else {
+    axios
+      .get(url)
+      .then((response) => {
+        const { temp: temperature } = response.data.main;
+        const { name: location } = response.data;
+        res.render('index', {
+          weather: `It is currently ${temperature} in ${location}`,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 };
 
 exports.renderAboutPage = (req, res) => {
