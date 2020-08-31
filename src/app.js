4844cb6c118ebce29fc86de1b8ea77b9244ea95b
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-08-31 14:55:41 +0200

    applied mvc pattern

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const router = require('./router');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -8,15 +9,7 @@ app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Express Weather Finder',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.use('/', router);
 
 app.listen(3000, () => {
   console.log('App running on port 3000');
